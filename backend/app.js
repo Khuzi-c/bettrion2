8ecf51a3442c0f650e14eb9d6a8fbb053015d4bc
Khxzi <khuzibutt15@gmail.com>
2026-01-15 00:59:46 +0400

    Set body size limit to 50mb for JSON and URL encoding
    Increase JSON and URL-encoded body size limit to 50mb.

--- backend/app.js
+++ backend/app.js
@@ -22,8 +22,8 @@ app.use(require('helmet')({
     crossOriginEmbedderPolicy: false
 }));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // DEBUG LOGGER & VISITOR LOGGER
 const visitorLogger = require('./middleware/visitorLogger');
